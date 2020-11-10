8e7fd5ae290d07f6dbd1860a128430338c996105
Jim <51420306+MrYouu@users.noreply.github.com>
2020-11-10 21:06:28 +0200

    Add files via upload

--- Files/Functions.js
+++ Files/Functions.js
@@ -460,6 +460,27 @@ function clearTestData()
     sessionStorage.removeItem("canTestEnd");
 }
 
+function getCookie(Name)
+{
+    var dc = document.cookie;
+    var prefix = Name + "=";
+    var begin = dc.indexOf("; " + prefix);
+
+    if (begin == -1)
+    {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) end = dc.length;
+    }
+    
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
 function defaultFunction()
 {
 
